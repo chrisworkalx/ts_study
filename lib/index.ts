d22fada7814bc50b448e2b3297155b3ba3068a8c
chrisworkalx <857717575@qq.com>
2020-07-15 22:45:18 +0800

    加法新增

--- lib/index.ts
+++ lib/index.ts
@@ -4,9 +4,9 @@
  * @Autor: yaomingfei
  * @Date: 2020-01-06 14:35:14
  * @LastEditors: chrisworkalx
- * @LastEditTime: 2020-07-15 22:14:54
+ * @LastEditTime: 2020-07-15 22:44:50
  */
 // # 非常简单的加法函数
 export function add(a:number, b:number) : number {
-  return a + b + 123;
+  return a + b + 123 + 456;
 }
